4893c326e830d3aa1513cde7856b3d8b9a3a117e
YannLysias <lysiasyannloemba06@gmail.com>
2024-03-05 14:20:59 +0100

    7eme commit

--- resources/views/adherant.blade.php
+++ resources/views/adherant.blade.php
@@ -214,6 +214,8 @@
 
     <!-- Custom Theme Scripts -->
     <script src="/build/js/custom.min.js"></script>
+
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
